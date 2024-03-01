5c1e71a1d6b4dec2a83b8164e5150fc42e9f825e
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-01 10:37:56 +0900

    Feat : ServerCommunicatorInter 인터페이스 구현

---
+++ Peer/src/main/java/com/peer/servercommunication/ServerCommunicatorInter.java
@@ -0,0 +1,14 @@
+package com.peer.servercommunication;
+
+import java.util.List;
+
+public interface ServerCommunicatorInter {
+
+    void createChattingRoom(String roomName, int maximum);
+
+    List<Peer> connectChattingRoom(String roomName);
+
+    void disconnectChattingRoom();
+
+    void getCurrentChattingRoom();
+}
