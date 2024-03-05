41a4c80c59e4c97faf4b2f4ac8d99fa47fec38e1
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-05 20:55:12 +0900

    Feat : convertJsonStringToMessage() 메소드 추가

--- Peer/src/main/java/com/util/JsonParser.java
+++ Peer/src/main/java/com/util/JsonParser.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.servercommunication.Peer;
 import java.util.Map;
 import java.util.Map.Entry;
+import com.message.*;
 
 /**
  * Json Parsing에 대한 메소드를 모아놓은 Util 클래스입니다.
@@ -44,6 +45,14 @@ public class JsonParser {
         return objectMapper.readTree(json);
     }
 
+    /**
+     * <p>JsonNode 객체를 Peer 객체로 변환시키는 메소드</p>
+     * <p>이때, Peer 객체로 변경 실패 시 예외를 발생시킵니다.</p>
+     *
+     * @param json Peer 객체로 변환시키고자 하는 JsonNode 객체
+     * @return 변환된 Peer 객체
+     * @throws JsonProcessingException
+     */
     public static Peer convertJsonNodeToPeer(JsonNode json) throws JsonProcessingException {
         return objectMapper.readValue(json.toString(), Peer.class);
     }
@@ -62,5 +71,66 @@ public class JsonParser {
         }
 
         return retVal;
+    }
+
+    /**
+     * <p>messageType에 맞는 객체로 Json String을 변환시킵니다.</p>
+     * <p>지정한 messageType과 다른 messageType이 들어온 경우 ResultResponse 객체로 변환시킵니다.</p>
+     * <p>Parsing 도중 문제가 생기면 null을 반환합니다.</p>
+     * <p>---------------------------------------------</p>
+     * <p>지정된 messageType: {ConnectMessage, ConnectResponse, CreateMessage, CurrentChattingRoomResponse, DisconnectMessage, TalkMessage, ResultResponse}</p>
+     *  <p>---------------------------------------------</p>
+     *
+     * @param json 변환하고자 하는 Json String
+     * @param messageType 변환하고자 하는 메시지 객체 타입 이름
+     * @return 변환된 Message 객체 / Parsing 도중 문제가 생길 경우 null
+     */
+    public static Message convertJsonStringToMessage(String json, String messageType) {
+        Message retVal = null;
+
+        try {
+            retVal = switch (messageType) {
+                case "ConnectMessage" -> convertJsonStringToConnectMessage(json);
+                case "ConnectResponse" -> convertJsonStringToConnectResponse(json);
+                case "CreateMessage" -> convertJsonStringToCreateMessage(json);
+                case "CurrentChattingRoomResponse" ->
+                    convertJsonStringToCurrentChattingRoomResponse(json);
+                case "DisconnectMessage" -> convertJsonStringToDisconnectMessage(json);
+                case "TalkMessage" -> convertJsonStringToTalkMessage(json);
+                default -> convertJsonStringToResultResponse(json);
+            };
+        } catch (JsonProcessingException e) {
+            retVal = null;
+        }
+
+        return retVal;
+    }
+
+    private static ConnectMessage convertJsonStringToConnectMessage(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, ConnectMessage.class);
+    }
+
+    private static ConnectResponse convertJsonStringToConnectResponse(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, ConnectResponse.class);
+    }
+
+    private static CreateMessage convertJsonStringToCreateMessage(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, CreateMessage.class);
+    }
+
+    private static CurrentChattingRoomResponse convertJsonStringToCurrentChattingRoomResponse(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, CurrentChattingRoomResponse.class);
+    }
+
+    private static DisconnectMessage convertJsonStringToDisconnectMessage(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, DisconnectMessage.class);
+    }
+
+    private static TalkMessage convertJsonStringToTalkMessage(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, TalkMessage.class);
+    }
+
+    private static ResultResponse convertJsonStringToResultResponse(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, ResultResponse.class);
     }
 }
