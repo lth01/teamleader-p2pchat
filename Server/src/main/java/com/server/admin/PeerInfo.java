0645910cb5fecfa9820adaa29f3fda9031bc4694
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-14 19:00:50 +0900

    Feat : PeerInfo 클래스 추가

--- Server/src/main/java/com/server/admin/PeerInfo.java
+++ Server/src/main/java/com/server/admin/PeerInfo.java
@@ -1,6 +1,8 @@
 package com.server.admin;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +16,14 @@ public class PeerInfo {
     private String ip;
     private int port;
     private Long currentChattingRoomId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public PeerInfo(String nickname, String ip, int port, Long currentChattingRoomId) {
+        this.nickname = nickname;
+        this.ip = ip;
+        this.port = port;
+        this.currentChattingRoomId = currentChattingRoomId;
+    }
 }
