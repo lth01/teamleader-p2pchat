f90f1bc1b6151d1272b45098e57772e43be03680
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-14 19:01:38 +0900

    Feat : PeerInfo 클래스 Mapper 추가

--- Server/src/main/java/com/server/repository/mappers/PeerInfoMapper.java
+++ Server/src/main/java/com/server/repository/mappers/PeerInfoMapper.java
@@ -1,8 +1,24 @@
 package com.server.repository.mappers;
 
+import com.server.admin.PeerInfo;
+import java.util.List;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PeerInfoMapper {
 
+    List<PeerInfo> getAllPeerInfo();
+
+    Optional<PeerInfo> getPeerInfo(PeerInfo peerInfo);
+
+    List<PeerInfo> getPeerInfosByChattingRoomId(String chattingRoomId);
+
+    Integer savePeerInfo(PeerInfo peerInfo);
+
+    Integer updatePeerInfo(PeerInfo peerInfo);
+
+    Integer deletePeerInfo(PeerInfo peerInfo);
+
+    Boolean isExistNicknameInChattingRoom(PeerInfo peerInfo);
 }
