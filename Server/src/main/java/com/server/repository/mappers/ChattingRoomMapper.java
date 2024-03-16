2b01f1b6951677a960589feb63f84dc95b1a7e44
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-16 20:23:14 +0900

    Feat : ChattingRoom 클래스 Mapper 추가

--- Server/src/main/java/com/server/repository/mappers/ChattingRoomMapper.java
+++ Server/src/main/java/com/server/repository/mappers/ChattingRoomMapper.java
@@ -1,8 +1,22 @@
 package com.server.repository.mappers;
 
+import com.server.admin.ChattingRoom;
+import java.util.List;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ChattingRoomMapper {
 
+    List<ChattingRoom> getAllChattingRoom();
+
+    Optional<ChattingRoom> getChattingRoom(ChattingRoom chattingRoom);
+
+    Integer createChattingRoom(ChattingRoom chattingRoom);
+
+    Integer updateChattingRoomInfo(ChattingRoom chattingRoom);
+
+    Integer deleteChattingRoom(String roomName);
+
+    Boolean isExistChattingRoom(String roomName);
 }
