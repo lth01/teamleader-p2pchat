7b2a84f9ab93305c25892797b0b11a5e3cff1284
itaehui <dlxogml11235@naver.com>
2024-03-18 17:26:45 +0900

    temp

--- Peer/src/main/java/com/main/PeerApplication.java
+++ Peer/src/main/java/com/main/PeerApplication.java
@@ -1,6 +1,7 @@
 package com.main;
 
 import com.peer.info.PeerInfo;
+import com.peer.message.TalkMessage;
 import com.peer.peerservice.PeerService;
 import com.peer.peerservice.PeerServiceImpl;
 import com.servercommunication.Peer;
@@ -13,6 +14,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.util.List;
 
 public class PeerApplication {
     public static void main(String[] args) throws IOException {
@@ -42,26 +48,46 @@ public class PeerApplication {
             switch (menuStr){
                 case "Menu" -> printMenus();
                 case "Rooms" -> getRooms();
-                case "EnterRoom" -> enterRoom();
+                case "EnterRoom" -> enterRoom(PeerService ,serverCommunicatorInter, br);
                 case "WriteMsg" -> writeMsg();
             }
         }
     }
 
-    void printMenus(){
-
+    static void printMenus(){
+        System.out.println("메뉴");
+        System.out.println("1. Rooms");
+        System.out.println("현재 남아있는 방을 확인합니다.");
+        System.out.println("2. EnterRoom");
+        System.out.println("방에 접속할 수 있습니다. 들어가려는 방의 이름을 입력합니다.");
+        System.out.println("3.WriteMessage");
+        System.out.println("메세지를 작성합니다. 방에 들어가있지 않은 경우 전송되지 않습니다.");
     }
 
-    void getRooms(){
+    static void getRooms(ServerCommunicatorInter sc){
+        List<String> roomList =  sc.getCurrentChattingRoom();
 
+        System.out.println("현재 접근 가능한 채팅방 목록입니다.");
+        for(String room : roomList){
+            System.out.println("채팅방 이름: " + room);
+        }
     }
 
-    void enterRoom(){
-
+    static void enterRoom(PeerService ps, ServerCommunicatorInter sc, BufferedReader br) throws IOException {
+        System.out.println("접속하려는 방의 이름을 입력해주세요.");
+        String roomName = br.readLine();
+        List<PeerInfo> list = sc.connectChattingRoom(roomName);
+        for(PeerInfo peerInfo : list){
+            ps.doConnect(new Socket(peerInfo.getIp(), peerInfo.getPort()), peerInfo);
+        }
     }
 
-    void writeMsg(){
-
+    static void writeMsg(PeerService ps, ServerCommunicatorInter sc, BufferedReader br, String nickName) throws IOException{
+        System.out.println("메세지를 입력해주세요");
+        LocalDateTime now = LocalDateTime.now();
+        String msg = br.readLine();
+        ps.sendMessgeToPeer(new TalkMessage(nickName,(now.getLong(ChronoField.EPOCH_DAY) * 86400000) + now.getLong(
+            ChronoField.MILLI_OF_DAY) , msg));
     }
 
 }
